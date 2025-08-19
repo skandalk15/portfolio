668568be20a6bbebc0d47355ae1ff7b75170e3ec
Soham Kandalkar <111929496+skandalk15@users.noreply.github.com>
2025-08-19 10:50:56 +0530

    Update intro.tsx
    Added new resume

--- Soham_Kandalkar_Portfolio/components/intro.tsx
+++ Soham_Kandalkar_Portfolio/components/intro.tsx
@@ -67,7 +67,7 @@ export default function Intro() {
             <a className='group bg-white px-7 py-3  text-gray-700
             flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110
            active:scale-105 transition cursor-pointer border border-black/10 dark:bg-white/10  dark:text-white/60' 
-           href='https://drive.google.com/file/d/1r8fjLqmgAixZv5F8SiGfLNs2TcDEQVd_/view?usp=sharing' download={true} >Download CV<HiDownload className='opacity-60 group-hover:translate-y-1 transition' /></a>
+           href='https://drive.google.com/file/d/1HvA25DXl48H6hk11KgwXx2jsd8KpFEeb/view?usp=sharing' download={true} >Download CV<HiDownload className='opacity-60 group-hover:translate-y-1 transition' /></a>
             
             <a className='bg-white p-4 text-gray-700
             flex items-center gap-2 rounded-full outline-none text-[1.35rem] focus:scale-[1.15] hover:scale-[1.15]
